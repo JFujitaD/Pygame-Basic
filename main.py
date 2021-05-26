e8a26d91cae351f6587495a7cb918a303832dd38
JFujitaD <fujitajulian@icloud.com>
2021-05-25 18:17:38 -0600

    Initialized Project

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame_manager import PygameManager
+
+def main():
+    """
+        Entry point for program that tests the PygameManager
+    """
+    pass
+
+if __name__ == '__main__':
+    main() 
